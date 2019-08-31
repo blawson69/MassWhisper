fa5c228756fc981d352df2fb7e1dd8b60826ab51
Ben L <39037350+blawson69@users.noreply.github.com>
2019-08-30 19:22:26 -0700

    Update MassWhisper.js
    Corrected Venmo link in README

--- MassWhisper.js
+++ MassWhisper.js
@@ -4,7 +4,7 @@ A Roll20 script to allow a GM to send a whisper to multiple characters
 
 On Github:	https://github.com/blawson69
 Contact me: https://app.roll20.net/users/1781274/ben-l
-Like this script? Buy me a coffee: https://venmo.com/theBenLawson
+Like this script? Buy me a coffee: https://venmo.com/theRealBenLawson
 */
 
 var MassWhisper = MassWhisper || (function () {
